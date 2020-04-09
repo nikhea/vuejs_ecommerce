4a40fb3cbc1f1cb079f903045061ca6af960067b
nikhea <imonikheaugbodaga@gmail.com>
2020-04-09 21:33:06 +0100

    added server.js and start script

---
+++ server.js
@@ -0,0 +1,10 @@
+// server.js
+const express = require('express');
+// var path = require('path');
+const serveStatic = require('serve-static');
+const app = express();
+app.use(serveStatic(__dirname + "/dist"));
+var port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log('vue serve started')
+});
